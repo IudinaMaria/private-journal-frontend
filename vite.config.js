3c53b09966f89f1618d25c7ad3bef5e9ed890b5f
Maria Iudina <gretarichter@gmail.com>
2025-06-26 21:54:31 +0300

    fix trigger: use master branch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 важно для корректной работы в S3/CloudFront
 })
