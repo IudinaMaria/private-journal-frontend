b0a959e1a2b0e0a1ccba12c4a7409b3b64dc35c1
Maria Iudina <gretarichter@gmail.com>
2025-06-27 21:24:28 +0300

    попытка номер 35747343748 найти ошибку

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,16 @@
-// aws-exports.js
-export default {
+const awsExports = {
   Auth: {
     region: "eu-north-1",
-    userPoolId: "eu-north-1_vcXKxrYk5",
-    userPoolWebClientId: "21tgfqv26mks41gscps8g1fi7b",
-    mandatorySignIn: true,
+    userPoolId: "eu-north-1_vcXKxrYk5", // ← тот, что ты уже используешь
+    userPoolWebClientId: "21tgfqv26mks41gscps8g1fi7b", // ← твой app client ID
+    oauth: {
+      domain: "eu-north-1vcxkxryk5.auth.eu-north-1.amazoncognito.com", // ← ТОТ САМЫЙ!
+      scope: ["openid", "email", "profile"],
+      redirectSignIn: "https://d1bdaso729tx0i.cloudfront.net/",
+      redirectSignOut: "https://d1bdaso729tx0i.cloudfront.net/",
+      responseType: "code"
+    }
   }
 };
+
+export default awsExports;
