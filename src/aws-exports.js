280fae1576390b2cb2512414c50cf11539622459
Maria Iudina <gretarichter@gmail.com>
2025-06-27 21:34:49 +0300

    попытка номер 35747343748 найти ошибку

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsExports = {
     userPoolWebClientId: "21tgfqv26mks41gscps8g1fi7b", // ← твой app client ID
     oauth: {
       domain: "eu-north-1vcxkxryk5.auth.eu-north-1.amazoncognito.com", // ← ТОТ САМЫЙ!
-      scope: "email openid",  // ← ✅ только эти два!
+      scope: ["email", "openid"],
       redirectSignIn: "https://d1bdaso729tx0i.cloudfront.net/",
       redirectSignOut: "https://d1bdaso729tx0i.cloudfront.net/",
       responseType: "code"
